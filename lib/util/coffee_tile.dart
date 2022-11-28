4373446eeca4dfadb8cd6525eed07ed31f2ccc47
mohamed.aziz.mhadhbi <mohamedaziz.mhadhbi@esprit.tn>
2022-11-28 02:37:35 +0100

    working on structure of coffee tile

--- lib/util/coffee_tile.dart
+++ lib/util/coffee_tile.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 class CoffeeTile extends StatelessWidget {
@@ -8,12 +10,44 @@ class CoffeeTile extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.only(left: 25.0, bottom: 25),
       child: Container(
+        padding: EdgeInsets.all(12),
         width: 200,
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(12),
           color: Colors.black54,
         ),
-        child: Image.asset('lib/images/'),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // coffee image
+            ClipRRect(
+              borderRadius: BorderRadius.circular(12),
+              child: Image.asset('lib/images/latte-coffee-beans-side-view.jpg'),
+            ),
+
+            // coffee name
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 12.0, horizontal: 8),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Latte',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  SizedBox(
+                    height: 4,
+                  ),
+                  Text(
+                    "With Almond Milk",
+                    style: TextStyle(color: Colors.grey[700]),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
