1aceea69dd024933161185c787f922e43af2387a
mohamed.aziz.mhadhbi <mohamedaziz.mhadhbi@esprit.tn>
2022-11-29 05:20:10 +0100

    making linear gradient background for the splash screen

---
+++ lib/splash_screen.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topRight,
+            end: Alignment.bottomRight,
+            colors: [
+              Color(0xffff8000),
+              Color(0xffce1010),
+            ]
+          ),
+        ),
+        child: Column(children: [Image.asset('lib/images/')],),
+      ),
+    );
+  }
+}
